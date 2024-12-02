af3f528ed9bb4cb72f41127fcd86d7b07e478446
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-02 21:35:31 +0600

    custom_button.dart: add

---
+++ lib/features/auth/login/presentation/widgets/custom_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton(
+      {super.key,
+      required this.size,
+      required this.text,
+      required this.onPressed,
+      required this.bgColor,
+      required this.fgColor,
+      required this.isLoading});
+  final Size size;
+  final String text;
+  final VoidCallback onPressed;
+  final Color bgColor;
+  final Color fgColor;
+  final bool isLoading;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: size.height,
+      width: size.width,
+      child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: bgColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10)
+            ),
+          ),
+          child: isLoading
+              ? CupertinoActivityIndicator(
+                  radius: 12,
+                  color: fgColor,
+                )
+              : Text(
+                  text,
+                  style: TextStyle(color: fgColor,fontWeight: FontWeight.bold),
+                )),
+    );
+  }
+}
