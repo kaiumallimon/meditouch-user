63d55b73f7dbe3eb83dbd66575175f225bee2262
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-02 23:43:40 +0600

    register event, state, bloc add

---
+++ lib/features/auth/register/logic/register_bloc.dart
@@ -0,0 +1,3 @@
+import 'package:equatable/equatable.dart';
+import 'package:image_picker/image_picker.dart';
+part './register_event.dart';
---
+++ lib/features/auth/register/logic/register_event.dart
@@ -0,0 +1,37 @@
+part of 'register_bloc.dart';
+
+abstract class RegisterEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class RegisterSubmitted extends RegisterEvent {
+  final String name;
+  final String phone;
+  final String email;
+  final String gender;
+  final DateTime dob;
+  final String password;
+  final XFile image;
+
+  RegisterSubmitted(
+      {required this.name,
+      required this.phone,
+      required this.email,
+      required this.gender,
+      required this.dob,
+      required this.password,
+      required this.image});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+    this.name,
+    this.phone,
+    this.email,
+    this.gender,
+    this.dob,
+    this.password,
+    this.image
+  ];
+}
