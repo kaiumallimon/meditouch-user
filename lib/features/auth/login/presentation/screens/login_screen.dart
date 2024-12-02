2ebe74679b61933ced4849dbfe6e5ff7c44ebcc5
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-02 21:36:12 +0600

    login_screen.dart: design complete

--- lib/features/auth/login/presentation/screens/login_screen.dart
+++ lib/features/auth/login/presentation/screens/login_screen.dart
@@ -1,28 +1,200 @@
-// file: login_screen.dart
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
+import 'package:meditouch/common/widgets/gradient_bg.dart';
+import 'package:meditouch/features/auth/login/presentation/widgets/continue_with_google.dart';
+import 'package:meditouch/features/auth/login/presentation/widgets/custom_button.dart';
+import 'package:meditouch/features/auth/login/presentation/widgets/custom_passwordfield.dart';
+import 'package:meditouch/features/auth/login/presentation/widgets/custom_textfield.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
+
 
   @override
   Widget build(BuildContext context) {
-    // retain the status bar
+    // Retain the status bar
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-    
-    // get theme
+
+    // Get theme
     final theme = Theme.of(context).colorScheme;
-    
-    // set status bar and nav bar colors:
+
+    // Set status bar and nav bar colors:
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: theme.surface,
-      statusBarIconBrightness: theme.brightness,
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
       systemNavigationBarColor: theme.surface,
-      systemNavigationBarIconBrightness: theme.brightness
+      systemNavigationBarIconBrightness: theme.brightness,
     ));
+
+    // Get device height
+    final height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: SafeArea(child: Column()),
-    );
+        body: Stack(
+      fit: StackFit.expand,
+      children: [
+        GradientBackground(),
+        Positioned(
+          top: (height * .10),
+          left: 0,
+          right: 0,
+          child: Image.asset(
+            'assets/images/logo2.png',
+            height: 130,
+            width: 130,
+          ),
+        ),
+        Positioned(
+            top: height * .35,
+            left: 0,
+            right: 0,
+            child: Container(
+              width: double.infinity,
+              height: height * .65,
+              padding: const EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                  color: theme.surface,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  )),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        text: 'Sign in to your\n',
+                        style: GoogleFonts.lexend(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'MediTouch',
+                            style: GoogleFonts.lexend(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .primary, // Highlight color
+                            ),
+                          ),
+                          TextSpan(
+                            text: ' Account',
+                            style: GoogleFonts.lexend(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.onSurface,
+                            ),
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    CustomTextfield(
+                        hint: "Email Address",
+                        size: const Size(350, 50),
+                        bgColor: theme.primary.withOpacity(.15),
+                        fgColor: theme.onSurface,
+                        controller: emailController),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    CustomPasswordfield(
+                        hint: "Password",
+                        size: const Size(350, 50),
+                        bgColor: theme.primary.withOpacity(.15),
+                        fgColor: theme.onSurface,
+                        controller: passwordController),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        GestureDetector(
+                          onTap: () {},
+                          child: Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                              color: theme.error,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 14
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    CustomButton(
+                        size: Size(350, 50),
+                        text: "Sign in",
+                        onPressed: () {},
+                        bgColor: theme.primary,
+                        fgColor: theme.onPrimary,
+                        isLoading: false),
+
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Don't have an account?"),
+                        const SizedBox(width: 8,),
+                        Text("Create an account",style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: theme.primary
+                        ),)
+                      ],
+                    ),
+
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Or",style: TextStyle(
+                          color: theme.onSurface.withOpacity(.5)
+                        ),),
+                      ],
+                    ),
+
+
+
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+
+                    CustomGoogleButton(
+                        size: Size(350, 50),
+                        text: "Continue with Google",
+                        onPressed: () {},
+                        bgColor: theme.primary.withOpacity(.2),
+                        fgColor: theme.onSurface,
+                        isLoading: false),
+
+                  ],
+                ),
+              ),
+            ))
+      ],
+    ));
   }
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 }
