e5e7e12801c8e4fb924665e390302f50b472a0fd
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-07 15:29:06 +0600

    epharmacy page add (initial)

---
+++ lib/features/dashboard/features/epharmacy/presentation/screens/epharmacy_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EpharmacyScreen extends StatelessWidget {
+  const EpharmacyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Epharmacy Screen'),
+    );
+  }
+}
