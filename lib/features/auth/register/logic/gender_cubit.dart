931f9f5fd6981df12fd01b92b0a70b1a843cc343
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-28 04:27:48 +0600

    refactor: update import for bloc to flutter_bloc in GenderCubit

--- lib/features/auth/register/logic/gender_cubit.dart
+++ lib/features/auth/register/logic/gender_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class GenderCubit extends Cubit<String?> {
   GenderCubit() : super(null); // Initial state is null
