1b4ccf52040a37a386c76e8f1b433a68ec5af0e0
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-28 04:27:32 +0600

    feat: integrate Firebase authentication and Firestore for user login functionality

--- lib/features/auth/login/data/repository/login_repository.dart
+++ lib/features/auth/login/data/repository/login_repository.dart
@@ -1,55 +1,94 @@
-import 'dart:convert';
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
 import 'package:meditouch/common/repository/hive_repository.dart';
 
 class LoginRepository {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   Future<Map<String, dynamic>> login(String email, String password) async {
-    const String apiUrl = 'https://meditouch.bcrypt.website/auth/login';
-
-    // Prepare the request body
-    final Map<String, String> requestBody = {
-      'email': email,
-      'password': password,
-    };
-
-    // Make the POST request
     try {
-      final response = await http.post(
-        Uri.parse(apiUrl),
-        headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': dotenv.env['X_API_KEY']!
-        },
-        body: json.encode(requestBody),
+      // Sign in the user
+      final UserCredential userCredential =
+          await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
       );
 
-      // Check if the response is successful (status code 200)
-      if (response.statusCode == 200) {
-        // Parse and return the response body as a Map
-        return json.decode(response.body);
-      } else {
-        return json.decode(response.body);
+      // Get the user
+      final User? user = userCredential.user;
+
+      // Check if the user is null
+      if (user == null) {
+        return {
+          "status": "error",
+          "error": "User not found. Please check your credentials."
+        };
       }
+
+      // Get the user data from Firestore
+      final DocumentSnapshot snapshot = await _firestore
+          .collection('db_client_user_userinfo')
+          .doc(user.uid)
+          .get();
+
+      // Check if the document exists
+      if (!snapshot.exists) {
+        return {
+          "status": "error",
+          "error": "User data not found in the database."
+        };
+      }
+
+      // Cast the data to a map
+      final Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
+
+      // Add UID to the data
+      data['id'] = user.uid;
+
+      // Return the user data
+      return {"status": "success", "message": "Login successful", "user": data};
     } catch (e) {
-      return {'message': e};
+      // Catch different types of errors and provide more specific error messages
+      if (e is FirebaseAuthException) {
+        return {
+          "status": "error",
+          "error": e.message ?? "An unknown error occurred."
+        };
+      } else if (e is FirebaseException) {
+        return {
+          "status": "error",
+          "error":
+              "An error occurred while connecting to the Firestore database."
+        };
+      } else {
+        return {
+          "status": "error",
+          "error": "An unexpected error occurred. Please try again."
+        };
+      }
     }
   }
 
   Future<void> logout(BuildContext context) async {
-    await HiveRepository().deleteUserInfo();
+    try {
+      await HiveRepository().deleteUserInfo();
 
-    final theme = Theme.of(context).colorScheme;
+      final theme = Theme.of(context).colorScheme;
 
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.light,
-      systemNavigationBarColor: theme.surface,
-      systemNavigationBarIconBrightness: theme.brightness,
-    ));
-    Navigator.of(context).pushReplacementNamed('/login');
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+        systemNavigationBarColor: theme.surface,
+        systemNavigationBarIconBrightness: theme.brightness,
+      ));
+      Navigator.of(context).pushReplacementNamed('/login');
+    } catch (e) {
+      // Handle errors during logout
+      print('Error during logout: $e');
+      // Optionally, you can show a message or log the error
+    }
   }
 }
