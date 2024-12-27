a910cde794adbc9087ca4416c83be46e2ee49203
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-28 05:34:50 +0600

    refactor: update CustomFloatingNavigationBar icons and labels for consistency

--- lib/features/dashboard/navigation/navbar/custom_navbar.dart
+++ lib/features/dashboard/navigation/navbar/custom_navbar.dart
@@ -11,29 +11,55 @@ class CustomFloatingNavigationBar extends StatelessWidget {
     return BlocBuilder<NavigationCubit, int>(
       builder: (context, currentIndex) {
         return NavigationBar(
-          surfaceTintColor: Theme.of(context).colorScheme.primary,
-          indicatorColor: Theme.of(context).colorScheme.primary,
-          backgroundColor: Theme.of(context).colorScheme.surface,
           selectedIndex: currentIndex,
           onDestinationSelected: (int index) {
             BlocProvider.of<NavigationCubit>(context).switchTo(index);
           },
-          destinations: const <Widget>[
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          surfaceTintColor:
+              Theme.of(context).colorScheme.primary.withOpacity(.15),
+          indicatorColor: Theme.of(context).colorScheme.primary,
+          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+          destinations: [
             NavigationDestination(
-              icon: Icon(RIcon.Home_),
+              icon: Icon(Icons.home_outlined),
+              selectedIcon: Icon(
+                Icons.home,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
               label: 'Home',
             ),
             NavigationDestination(
-              icon: Icon(RIcon.Medical_Kit),
-              label: 'EPharmacy',
+              icon: Icon(Icons.medical_services_outlined),
+              selectedIcon: Icon(
+                Icons.medical_services,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+              label: 'E-Pharmacy',
             ),
             NavigationDestination(
-              icon: Icon(Icons.bookmark_border),
-              label: 'Appointments',
+              icon: Icon(Icons.bookmark_outline),
+              selectedIcon: Icon(
+                Icons.bookmark,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+              label: 'Appts',
             ),
             NavigationDestination(
-              icon: Icon(RIcon.Chat_Round_),
-              label: 'Messages',
+              icon: Icon(Icons.chat_outlined),
+              selectedIcon: Icon(
+                Icons.chat,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+              label: 'Inbox',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.account_circle_outlined),
+              selectedIcon: Icon(
+                Icons.account_circle,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+              label: 'Account',
             ),
           ],
         );
