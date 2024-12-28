039474c58ae1bb8acbed73e217a42816bc64808c
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-29 02:22:34 +0600

    feat: enhance HiveRepository with user info management methods

--- lib/common/repository/hive_repository.dart
+++ lib/common/repository/hive_repository.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -5,6 +6,13 @@ class HiveRepository {
   static const String _userInfoBox = 'userInfoBox';
   static const String _userInfoKey = 'userInfo';
 
+
+  // ValueListenable to listen for changes in the user info box
+  ValueListenable<Box<Map>> get userInfoBoxListener {
+    return Hive.box<Map>(_userInfoBox).listenable();
+  }
+
+  // Initialize the Hive box
   Future<void> init() async {
     final appDocumentDir = await getApplicationDocumentsDirectory();
     Hive.init(appDocumentDir.path);
@@ -15,6 +23,7 @@ class HiveRepository {
     }
   }
 
+  // Save user info to Hive
   Future<void> saveUserInfo(String id, String name, String email, String gender,
       String dob, String image, String phone) async {
     final userInfoBox = Hive.box<Map>(_userInfoBox);
@@ -30,6 +39,7 @@ class HiveRepository {
     await userInfoBox.put(_userInfoKey, userInfo);
   }
 
+  // Retrieve user info from Hive
   Future<Map<String, dynamic>?> getUserInfo() async {
     final userInfoBox = Hive.box<Map>(_userInfoBox);
     final dynamic userInfo = userInfoBox.get(_userInfoKey);
@@ -40,8 +50,20 @@ class HiveRepository {
     return null;
   }
 
+  // Delete user info from Hive
   Future<void> deleteUserInfo() async {
     final userInfoBox = Hive.box<Map>(_userInfoBox);
     await userInfoBox.delete(_userInfoKey);
+  }
+
+  // Update user info in Hive
+  Future<void> updateUserInfo(Map<String, dynamic> updatedData) async {
+    final userInfoBox = Hive.box<Map>(_userInfoBox);
+    final userInfo = userInfoBox.get(_userInfoKey);
+
+    if (userInfo is Map) {
+      userInfo.addAll(updatedData);
+      await userInfoBox.put(_userInfoKey, userInfo);
+    }
   }
 }
