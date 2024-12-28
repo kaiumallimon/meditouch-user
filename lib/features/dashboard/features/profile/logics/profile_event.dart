bb1fad50abcde6f5437281da8b39776e668a7299
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-29 02:22:06 +0600

    feat: add ProfileEvent classes for handling profile-related events in the application

---
+++ lib/features/dashboard/features/profile/logics/profile_event.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+import 'package:image_picker/image_picker.dart';
+
+abstract class ProfileEvent extends Equatable {
+  const ProfileEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class UpdateProfile extends ProfileEvent {
+  final Map<String, dynamic> data;
+  final String uid;
+  final XFile? image;
+
+  const UpdateProfile(
+      {required this.data, required this.uid, this.image});
+
+  @override
+  List<Object> get props => [data, uid];
+}
+
+class ProfileEdit extends ProfileEvent {
+  const ProfileEdit();
+
+  @override
+  List<Object> get props => [];
+}
+
+class ProfileEditDone extends ProfileEvent {
+  const ProfileEditDone();
+
+  @override
+  List<Object> get props => [];
+}
