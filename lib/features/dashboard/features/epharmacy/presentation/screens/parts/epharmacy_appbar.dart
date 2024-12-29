2b0a52feae80b627e05580e25dfcdcc9d758b973
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-30 03:14:41 +0600

    feat: add custom AppBar widget for E-Pharmacy screen

---
+++ lib/features/dashboard/features/epharmacy/presentation/screens/parts/epharmacy_appbar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+Widget buildCustomAppBar(BuildContext context, ColorScheme theme) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text('E-Pharmacy',
+          style: TextStyle(
+              height: 1,
+              color: theme.primary,
+              fontSize: 20,
+              fontWeight: FontWeight.bold)),
+      const SizedBox(height: 5),
+      Text('Powered by MedEasy',
+          style:
+              TextStyle(color: theme.onSurface.withOpacity(.5), fontSize: 14)),
+    ],
+  );
+}
