4a33de1be78937020e96504a58434e482381ede1
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-30 04:42:13 +0600

    feat: add utility function to generate detailed medicine page URL in E-Pharmacy

--- lib/common/utils/epharmacy_util.dart
+++ lib/common/utils/epharmacy_util.dart
@@ -17,3 +17,13 @@ double getDiscountedPrice(double unitPrice, medicine) {
 int getNextPage(EpharmacySuccessState state) {
   return (state.medicines.length ~/ 20) + 1;
 }
+
+String getDetailedPageUrl(
+    String id, String genericName, String categoryName, String strength) {
+  var formattedGenericName = genericName.replaceAll(' ', '%20');
+  var formattedCategoryName = categoryName.replaceAll(' ', '%20');
+  var formattedStrength = strength.replaceAll(' ', '%20');
+  String baseUrl =
+      "https://api.medeasy.health/api/patient/medicine-details/?generic_name=$formattedGenericName&category_name=$formattedCategoryName&id=${id.toString()}&strength=$formattedStrength&lang=en";
+  return baseUrl;
+}
