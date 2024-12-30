b7642e39b33b3b3cda856cf24a28dc8548d4b8c7
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-30 08:17:55 +0600

    feat: add detailed medicine unit selector dropdown for price unit selection

---
+++ lib/features/dashboard/features/epharmacy/presentation/screens/parts/detailed_medicine_price_unit_dropdown.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../logics/detailed_medicine_bloc.dart';
+import '../../../logics/detailed_medicine_event.dart';
+import '../../../logics/detailed_medicine_state.dart';
+
+DropdownButton<String> detailedMedicineUnitSelectorDropDown(
+    DetailedMedicineSuccess state, ColorScheme theme, BuildContext context) {
+  return DropdownButton<String>(
+    value: state.medicineDetails.unitPrices[state.selectedUnitIndex].unit,
+    icon: Icon(Icons.arrow_drop_down_rounded,
+        color: theme.onSurface.withOpacity(.5)),
+    iconSize: 24,
+    elevation: 16,
+    style: TextStyle(
+        fontFamily: 'SF-Pro-Text',
+        color: theme.onSurface,
+        fontSize: 14,
+        fontWeight: FontWeight.w500),
+    underline: Container(
+      height: 0,
+      color: theme.primary,
+    ),
+    onChanged: (String? newValue) {
+      context.read<DetailedMedicineBloc>().add(ChangeUnitRequested(state
+          .medicineDetails.unitPrices
+          .indexWhere((element) => element.unit == newValue)));
+    },
+    items: state.medicineDetails.unitPrices
+        .map<DropdownMenuItem<String>>((unitPrice) {
+      return DropdownMenuItem<String>(
+        value: unitPrice.unit,
+        child: Text(unitPrice.unit),
+      );
+    }).toList(),
+  );
+}
+
