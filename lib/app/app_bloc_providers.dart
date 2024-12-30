5e21d414ceda47f6366ffd6b50e336768a1c41dd
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-30 08:16:22 +0600

    feat: add app_bloc_providers for managing various blocs and cubits in the application

---
+++ lib/app/app_bloc_providers.dart
@@ -0,0 +1,52 @@
+import './app_exporter.dart';
+
+final providers = [
+  // splash bloc
+  BlocProvider<SplashBloc>(create: (_) => SplashBloc()),
+  // welcome cubit
+  BlocProvider<WelcomeCubit>(create: (_) => WelcomeCubit()),
+  // register cubits
+  BlocProvider<DateCubit>(create: (_) => DateCubit()),
+  // gender cubit
+  BlocProvider<GenderCubit>(create: (_) => GenderCubit()),
+  // imagepicker cubit
+  BlocProvider<ImagePickerCubit>(create: (_) => ImagePickerCubit()),
+  // register bloc
+  BlocProvider<RegisterBloc>(
+    create: (_) => RegisterBloc(),
+  ),
+
+  // login bloc
+  BlocProvider<LoginBloc>(
+    create: (_) => LoginBloc(loginRepository: LoginRepository()),
+  ),
+  // navigation cubit
+  BlocProvider<NavigationCubit>(create: (_) => NavigationCubit()),
+
+  // home bloc
+  BlocProvider<HomeBloc>(
+      create: (_) => HomeBloc(HiveRepository())..add(HomeRefreshRequested())),
+
+  // account bloc
+  BlocProvider<AccountBloc>(
+      create: (_) => AccountBloc(hiveRepository: HiveRepository())
+        ..add(const AccountRefreshRequested())),
+
+  // theme cubit
+  BlocProvider<ThemeCubit>(create: (_) => ThemeCubit()),
+
+  // profile bloc
+  BlocProvider<ProfileBloc>(
+    create: (_) => ProfileBloc(
+      profileRepository: ProfileRepository(),
+    ),
+  ),
+
+  // ephearmacy bloc
+  BlocProvider<EpharmacyBloc>(
+    create: (_) => EpharmacyBloc(),
+  ),
+
+  // add the detailed medicine bloc
+  BlocProvider<DetailedMedicineBloc>(create: (_) => DetailedMedicineBloc()),
+];
