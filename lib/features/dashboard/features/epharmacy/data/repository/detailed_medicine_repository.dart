31dd5013eabc862040b570b87203d5f149b693a8
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-30 08:16:53 +0600

    feat: add logging for medicine details retrieval in DetailedMedicineRepository

--- lib/features/dashboard/features/epharmacy/data/repository/detailed_medicine_repository.dart
+++ lib/features/dashboard/features/epharmacy/data/repository/detailed_medicine_repository.dart
@@ -1,13 +1,20 @@
+import 'dart:developer';
+
 import 'package:meditouch/features/dashboard/features/epharmacy/data/model/medicine_details_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class DetailedMedicineRepository {
   Future<MedicineDetailsModel?> getMedicineDetails(String url) async {
+    print('URL: $url');
+
     try {
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
-        return jsonDecode(response.body);
+        final json = jsonDecode(response.body);
+
+        log('Medicine Details: $json');
+        return MedicineDetailsModel.fromJson(json);
       } else {
         return null;
       }
