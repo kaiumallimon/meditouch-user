3343040cb6b96e16ced904687084f4767e3104d0
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-30 10:17:18 +0600

    feat: add search clear button functionality in Epharmacy search

---
+++ lib/features/dashboard/features/epharmacy/presentation/screens/parts/epharmacy_search_clear.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meditouch/features/dashboard/features/epharmacy/logics/epharmacy_search_bloc.dart';
+
+import '../../../logics/epharmacy_search_event.dart';
+
+ValueListenableBuilder<TextEditingValue> searchClear(
+    ColorScheme theme, TextEditingController searchController) {
+  return ValueListenableBuilder<TextEditingValue>(
+    valueListenable: searchController,
+    builder: (context, value, child) {
+      if (value.text.isNotEmpty) {
+        return IconButton(
+          icon: Icon(
+            Icons.clear,
+            color: theme.error,
+          ),
+          onPressed: () {
+            searchController.clear();
+            searchController.notifyListeners();
+
+            context.read<EpharmacySearchBloc>().add(
+                  EpharmacySearchClearEvent(),
+                );
+          },
+        );
+      }
+
+      return const SizedBox();
+    },
+  );
+}
