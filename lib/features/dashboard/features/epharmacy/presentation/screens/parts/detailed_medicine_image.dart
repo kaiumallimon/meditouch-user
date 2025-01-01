bc8e62905fe97ae89b4546ba033752d167e6525b
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-01 23:28:41 +0600

    feat: add placeholder image for detailed medicine view when no image is available

--- lib/features/dashboard/features/epharmacy/presentation/screens/parts/detailed_medicine_image.dart
+++ lib/features/dashboard/features/epharmacy/presentation/screens/parts/detailed_medicine_image.dart
@@ -1,17 +1,23 @@
 // ignore: non_constant_identifier_names
-  import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../../../common/utils/epharmacy_util.dart';
 import '../../../logics/detailed_medicine_state.dart';
 
 Widget BuildDetailedMedicineImage(DetailedMedicineSuccess state) {
-    return AspectRatio(
-      aspectRatio: 16 / 9,
-      child: CachedNetworkImage(
-        imageUrl: getImageUrl(state.medicineDetails.medicineImage),
-        width: double.infinity,
-        fit: BoxFit.cover,
-      ),
-    );
-  }
+  return AspectRatio(
+    aspectRatio: 16 / 9,
+    child: (state.medicineDetails.medicineImage == null ||
+            state.medicineDetails.medicineImage.isEmpty)
+        ? Image.asset(
+            'assets/images/medicine_placeholder.png',
+            fit: BoxFit.cover,
+          )
+        : CachedNetworkImage(
+            imageUrl: getImageUrl(state.medicineDetails.medicineImage),
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+  );
+}
