8345dfe7c905149487bf5b0aa6e689dd9d545d84
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-01 23:56:14 +0600

    feat: enhance CustomTintedIconButton to support optional count display

--- lib/common/widgets/custom_tinted_iconbutton.dart
+++ lib/common/widgets/custom_tinted_iconbutton.dart
@@ -1,24 +1,61 @@
-
 import 'package:flutter/material.dart';
 
 class CustomTintedIconButton extends StatelessWidget {
   const CustomTintedIconButton(
-      {super.key, required this.child, required this.onPressed});
+      {super.key,
+      required this.child,
+      required this.onPressed,
+      this.hasCount,
+      this.count});
   final Widget child;
   final Function() onPressed;
+  final bool? hasCount;
+  final int? count;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
+    return GestureDetector(
       onTap: onPressed,
-      borderRadius: BorderRadius.circular(10),
-      child: Container(
-        padding: const EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.primary.withOpacity(.1),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: child,
+      child: Stack(
+        clipBehavior: Clip.none, // To allow the counter to overflow
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            margin: EdgeInsets.only(
+              right: hasCount != null && hasCount! ? 10 : 0,
+            ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary.withOpacity(.1),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: child,
+          ),
+
+          // Count
+          if (hasCount != null && hasCount! && count != null)
+            Positioned(
+              right: 0,
+              top: 0,
+              child: Container(
+                padding: const EdgeInsets.all(
+                    6), // Adjust padding for the counter size
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: Text(
+                    count.toString(),
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
