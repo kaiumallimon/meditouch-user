a4ba01afa399ef54bc0a4b80fddf9b05e1d42e2c
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-02 14:26:09 +0600

    feat: enhance AppBar layout in E-Pharmacy screen for better usability and aesthetics

--- lib/features/dashboard/features/epharmacy/presentation/screens/parts/epharmacy_search_appbar.dart
+++ lib/features/dashboard/features/epharmacy/presentation/screens/parts/epharmacy_search_appbar.dart
@@ -7,48 +7,48 @@ import '../../../logics/epharmacy_search_event.dart';
 import 'epharmacy_search_clear.dart';
 
 AppBar searchAppBar(ColorScheme theme, BuildContext context,
-      TextEditingController searchController) {
-    return AppBar(
-              title: Container(
-                decoration: BoxDecoration(
-                  color: theme.surfaceContainer,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: TextField(
-                  controller: searchController,
-                  onChanged: (value) {
-                    (context as Element).markNeedsBuild();
-                  },
-                  onSubmitted: (value) async {
-                    final query = searchController.text;
-                    if (query.isNotEmpty) {
-                      await HiveRepository()
-                          .saveUserSearchHistory(query.toLowerCase());
+    TextEditingController searchController) {
+  return AppBar(
+    toolbarHeight: 70,
+    title: Container(
+      decoration: BoxDecoration(
+        color: theme.surfaceContainer,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextField(
+        controller: searchController,
+        onChanged: (value) {
+          (context as Element).markNeedsBuild();
+        },
+        onSubmitted: (value) async {
+          final query = searchController.text;
+          if (query.isNotEmpty) {
+            await HiveRepository().saveUserSearchHistory(query.toLowerCase());
 
-                      // Use context.read to get the bloc instance and add the event
-                      context.read<EpharmacySearchBloc>().add(
-                            EpharmacySearchQueryEvent(query: query),
-                          );
-                    }
-                  },
-                  decoration: InputDecoration(
-                    hintText: 'Search for medicines',
-                    hintStyle: TextStyle(
-                      color: theme.onSurface.withOpacity(.5),
-                    ),
-                    border: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                  ),
-                ),
-              ),
-              elevation: 0,
-              backgroundColor: theme.surfaceContainer,
-              surfaceTintColor: theme.surfaceContainer,
-              centerTitle: false,
-              actions: [
-                searchClear(theme, searchController),
-                const SizedBox(width: 10),
-              ],
-            );
-  }
+            // Use context.read to get the bloc instance and add the event
+            context.read<EpharmacySearchBloc>().add(
+                  EpharmacySearchQueryEvent(query: query),
+                );
+          }
+        },
+        decoration: InputDecoration(
+          hintText: 'Search for medicines',
+          hintStyle: TextStyle(
+            color: theme.onSurface.withOpacity(.5),
+          ),
+          border: InputBorder.none,
+          enabledBorder: InputBorder.none,
+          focusedBorder: InputBorder.none,
+        ),
+      ),
+    ),
+    elevation: 0,
+    backgroundColor: theme.surfaceContainer,
+    surfaceTintColor: theme.surfaceContainer,
+    centerTitle: false,
+    actions: [
+      searchClear(theme, searchController),
+      const SizedBox(width: 10),
+    ],
+  );
+}
