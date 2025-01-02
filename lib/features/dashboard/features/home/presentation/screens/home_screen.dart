c74e4f527cf5bd9ffcb32a79654dd3e03bc3cdcf
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-02 14:25:57 +0600

    feat: update HomeScreen layout for improved user interface and accessibility

--- lib/features/dashboard/features/home/presentation/screens/home_screen.dart
+++ lib/features/dashboard/features/home/presentation/screens/home_screen.dart
@@ -172,76 +172,79 @@ class HomeScreen extends StatelessWidget {
       floating: true,
       pinned: false,
       snap: false,
+      toolbarHeight: 60,
       elevation: 0,
       surfaceTintColor: Colors.transparent,
       leading: null,
       backgroundColor: theme.surfaceContainer,
       automaticallyImplyLeading: false,
-      flexibleSpace: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // User image in leading
-          Container(
-            decoration: BoxDecoration(
-              color: theme.primary.withOpacity(.1),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: CachedNetworkImage(
-                imageUrl: userData['image'],
-                height: 40,
-                width: 40,
-                fit: BoxFit.cover,
-                placeholder: (context, url) =>
-                    const CupertinoActivityIndicator(),
-                errorWidget: (context, url, error) =>
-                    const Icon(Icons.person), // Fallback icon
+      flexibleSpace: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // User image in leading
+            Container(
+              decoration: BoxDecoration(
+                color: theme.primary.withOpacity(.1),
+                borderRadius: BorderRadius.circular(10),
               ),
-            ),
-          ),
-          const Spacer(),
-
-          // Notification and search icons in actions
-          Row(
-            children: [
-              CustomTintedIconButton(
-                child: Image.asset(
-                  'assets/icons/notification.png',
-                  height: 25,
-                  width: 25,
-                  color: theme.primary,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: CachedNetworkImage(
+                  imageUrl: userData['image'],
+                  height: 40,
+                  width: 40,
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) =>
+                      const CupertinoActivityIndicator(),
+                  errorWidget: (context, url, error) =>
+                      const Icon(Icons.person), // Fallback icon
                 ),
-                onPressed: () {},
               ),
-              const SizedBox(width: 10),
-              BlocBuilder<HomeBloc, HomeState>(
-                builder: (context, state) {
-                  if (state is HomeLoaded) {
+            ),
+            const Spacer(),
+
+            // Notification and search icons in actions
+            Row(
+              children: [
+                CustomTintedIconButton(
+                  child: Image.asset(
+                    'assets/icons/notification.png',
+                    height: 25,
+                    width: 25,
+                    color: theme.primary,
+                  ),
+                  onPressed: () {},
+                ),
+                const SizedBox(width: 10),
+                BlocBuilder<HomeBloc, HomeState>(
+                  builder: (context, state) {
+                    if (state is HomeLoaded) {
+                      return CustomTintedIconButton(
+                        onPressed: () {
+                          Navigator.of(context).pushNamed('/cart');
+                        },
+                        hasCount: true,
+                        count: state.cartItemsCount,
+                        child: Icon(Icons.shopping_bag_outlined,
+                            color: theme.primary),
+                      );
+                    }
+
                     return CustomTintedIconButton(
                       onPressed: () {
                         Navigator.of(context).pushNamed('/cart');
                       },
-                      hasCount: true,
-                      count: state.cartItemsCount,
+                      hasCount: false,
                       child: Icon(Icons.shopping_bag_outlined,
                           color: theme.primary),
                     );
-                  }
-
-                  return CustomTintedIconButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed('/cart');
-                    },
-                    hasCount: false,
-                    child:
-                        Icon(Icons.shopping_bag_outlined, color: theme.primary),
-                  );
-                },
-              ),
-            ],
-          ),
-        ],
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
