2b225b097eeb105c2653a32ed14a7c3249ced64a
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-02 14:26:04 +0600

    feat: update AppBar layout in E-Pharmacy screen for improved spacing

--- lib/features/dashboard/features/epharmacy/presentation/screens/parts/epharmacy_appbar.dart
+++ lib/features/dashboard/features/epharmacy/presentation/screens/parts/epharmacy_appbar.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
 Widget buildCustomAppBar(BuildContext context, ColorScheme theme) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text('E-Pharmacy',
-          style: TextStyle(
-              height: 1,
-              color: theme.primary,
-              fontSize: 20,
-              fontWeight: FontWeight.bold)),
-      const SizedBox(height: 5),
-      Text('Powered by MedEasy',
-          style:
-              TextStyle(color: theme.onSurface.withOpacity(.5), fontSize: 12)),
-    ],
+  return Padding(
+    padding: const EdgeInsets.only(top: 0, bottom: 10),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('E-Pharmacy',
+            style: TextStyle(
+                height: 1,
+                color: theme.primary,
+                fontSize: 20,
+                fontWeight: FontWeight.bold)),
+        const SizedBox(height: 5),
+        Text('Powered by MedEasy',
+            style: TextStyle(
+                color: theme.onSurface.withOpacity(.5), fontSize: 12)),
+      ],
+    ),
   );
 }
