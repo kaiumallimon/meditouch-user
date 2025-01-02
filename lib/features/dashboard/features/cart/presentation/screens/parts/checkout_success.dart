d85a8c30641811c7d82d7a59e09669312b111e9f
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-02 14:30:16 +0600

    fix: remove redundant Navigator.pop call in checkout success screen

--- lib/features/dashboard/features/cart/presentation/screens/parts/checkout_success.dart
+++ lib/features/dashboard/features/cart/presentation/screens/parts/checkout_success.dart
@@ -4,39 +4,39 @@ import 'package:lottie/lottie.dart';
 import '../../../../../../../common/widgets/custom_button.dart';
 
 Center checkoutSuccess(ColorScheme theme, BuildContext context) {
-    return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Lottie.asset(
-                  'assets/lottie/success_.json',
-                  height: 100,
-                  width: 100,
-                  fit: BoxFit.contain,
-                  repeat: false,
-                ),
-                const SizedBox(height: 15),
-                Text(
-                  'Order Placed!',
-                  style: TextStyle(
-                    color: theme.primary,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                CustomButton(
-                  size: const Size(120, 45),
-                  text: "Exit",
-                  onPressed: () {
-                    Navigator.pop(context);
-                    Navigator.pop(context);
-                  },
-                  bgColor: theme.primary,
-                  fgColor: theme.onPrimary,
-                  isLoading: false,
-                ),
-              ],
-            ),
-          );
-  }
+  return Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Lottie.asset(
+          'assets/lottie/success_.json',
+          height: 100,
+          width: 100,
+          fit: BoxFit.contain,
+          repeat: false,
+        ),
+        const SizedBox(height: 15),
+        Text(
+          'Order Placed!',
+          style: TextStyle(
+            color: theme.primary,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 20),
+        CustomButton(
+          size: const Size(120, 45),
+          text: "Exit",
+          onPressed: () {
+            // Navigator.pop(context);
+            Navigator.pop(context);
+          },
+          bgColor: theme.primary,
+          fgColor: theme.onPrimary,
+          isLoading: false,
+        ),
+      ],
+    ),
+  );
+}
