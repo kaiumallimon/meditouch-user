bcc885475c9ff1d9795dc6e58cbe8d145b60c3f2
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-02 16:11:19 +0600

    feat: implement checkout web view for bKash payment processing with success and error handling

--- lib/features/dashboard/features/cart/presentation/screens/cart_checkout_screen.dart
+++ lib/features/dashboard/features/cart/presentation/screens/cart_checkout_screen.dart
@@ -1,9 +1,17 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meditouch/common/repository/bkash_repository.dart';
 import 'package:meditouch/common/widgets/custom_loading_animation.dart';
 import 'package:meditouch/features/dashboard/features/cart/models/cart_model.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
+import '../../../../../../common/repository/hive_repository.dart';
+import '../../../epharmacy/data/model/medicine_model.dart';
+import '../../../order/data/models/order_model.dart';
 import '../../logics/cart_bloc.dart';
+import '../../logics/cart_event.dart';
 import '../../logics/cart_state.dart';
 import 'parts/checkout_appbar.dart';
 import 'parts/checkout_body.dart';
@@ -38,8 +46,9 @@ class CartCheckoutScreen extends StatelessWidget {
             return checkoutSuccess(theme, context);
           }
 
-          if (state is CartPaymentSate) {
-            return Container();
+          if (state is CartPaymentWebview) {
+            return checkoutWebView(
+                theme, context, state, cartItems, selectedItems);
           }
 
           if (state is CartCheckoutError) {
@@ -56,4 +65,70 @@ class CartCheckoutScreen extends StatelessWidget {
       ),
     ));
   }
+}
+
+Widget checkoutWebView(
+    ColorScheme theme,
+    BuildContext context,
+    CartPaymentWebview state,
+    List<CartModel> cartItems,
+    List<String> selectedItems) {
+  return WebViewWidget(
+    controller: WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (String url) async {
+            if (url.contains('success')) {
+              print('Executing payment, please wait!');
+
+              try {
+                final response = await BkashRepository().executePaymentresponse(
+                  idToken: state.tokenId,
+                  paymentID: state.paymentId,
+                );
+
+                if (response.statusMessage == 'Successful') {
+                  BlocProvider.of<CartBloc>(context).add(PaymentExecuted(
+                      cartIds: selectedItems,
+                      paymentId: state.paymentId,
+                      transactionId: response.trxID,
+                      amount: response.amount,
+                      currency: response.currency,
+                      invoiceNumber: response.merchantInvoiceNumber,
+                      order: OrderModel(
+                          medicines: state.medicines,
+                          uid: state.uid,
+                          orderStatus: 'Processing',
+                          orderDate: DateTime.now(),
+                          deliveryAddress: state.deliveryAddress,
+                          userPhoneNumber: state.userPhone,
+                          userFullName: state.userFullName,
+                          paymentMethod: "bKash",
+                          paymentStatus: response.amount == state.totalAmount
+                              ? 'Paid'
+                              : 'Pending',
+                          totalPrice: state.totalAmount,
+                          orderId: "")));
+                } else {
+                  BlocProvider.of<CartBloc>(context)
+                      .add(CartCheckoutGotError(response.statusMessage));
+                }
+              } catch (e) {
+                BlocProvider.of<CartBloc>(context)
+                    .add(CartCheckoutGotError(e.toString()));
+              }
+            } else if (url.contains('failure')) {
+              print('Payment failed');
+              BlocProvider.of<CartBloc>(context)
+                  .add(CartCheckoutGotError('Payment failed'));
+            } else if (url.contains('cancelled')) {
+              BlocProvider.of<CartBloc>(context)
+                  .add(CartCheckoutGotError('Payment cancelled'));
+            }
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(state.paymentUrl)),
+  );
 }
