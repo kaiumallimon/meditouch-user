d3f41f75e3c878e76a5519508a70467d5fb833e8
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-08 06:34:09 +0600

    feat: update E-Pharmacy app bar layout and add navigation to GeminiMedicineScan

--- lib/features/dashboard/features/epharmacy/presentation/screens/parts/epharmacy_appbar.dart
+++ lib/features/dashboard/features/epharmacy/presentation/screens/parts/epharmacy_appbar.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:meditouch/common/widgets/custom_tinted_iconbutton.dart';
+import 'package:meditouch/features/dashboard/features/epharmacy/presentation/screens/gemini_medicine_scan.dart';
 
 Widget buildCustomAppBar(BuildContext context, ColorScheme theme) {
   return Padding(
     padding: const EdgeInsets.only(top: 0, bottom: 10),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text('E-Pharmacy',
-            style: TextStyle(
-                height: 1,
-                color: theme.primary,
-                fontSize: 20,
-                fontWeight: FontWeight.bold)),
-        const SizedBox(height: 5),
-        Text('Powered by MedEasy',
-            style: TextStyle(
-                color: theme.onSurface.withOpacity(.5), fontSize: 12)),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('E-Pharmacy',
+                style: TextStyle(
+                    height: 1,
+                    color: theme.primary,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold)),
+            const SizedBox(height: 5),
+            Text('Powered by MedEasy',
+                style: TextStyle(
+                    color: theme.onSurface.withOpacity(.5), fontSize: 12)),
+          ],
+        ),
+        CustomTintedIconButton(
+            child: Image.asset(
+              'assets/icons/ai.png',
+              width: 20,
+              height: 20,
+              color: theme.primary,
+            ),
+            onPressed: () {
+              // go to ai image screen page:
+              Get.to(GeminiMedicineScan());
+            })
       ],
     ),
   );
