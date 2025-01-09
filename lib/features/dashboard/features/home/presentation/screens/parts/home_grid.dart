603ae0746198bb984380cf076186dadb1fdd1f0a
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-10 01:04:59 +0600

    feat: add HomeGridMenu widget for enhanced dashboard navigation

---
+++ lib/features/dashboard/features/home/presentation/screens/parts/home_grid.dart
@@ -0,0 +1,94 @@
+import 'package:meditouch/app/app_exporter.dart';
+
+import '../../widgets/home_grid_item.dart';
+
+Widget buildHomeGridMenu(BuildContext context, ColorScheme theme) {
+  return SizedBox(
+    height: 240,
+    child: GridView.count(
+        physics: NeverScrollableScrollPhysics(),
+        crossAxisCount: 3,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        children: [
+          HomeGridItem(
+            title: 'Doctors',
+            icon: Image.asset(
+              'assets/icons/doctor-flaticon.png',
+              width: 25,
+              height: 25,
+            ),
+            onTap: () {
+              // Navigator.of(context).pushNamed(Routes.appointment);
+            },
+            backgroundColor: theme.primaryContainer,
+            textColor: theme.onSurface,
+          ),
+          HomeGridItem(
+            title: 'Appointments',
+            icon: Image.asset(
+              'assets/icons/doctor-flaticon.png',
+              width: 25,
+              height: 25,
+            ),
+            onTap: () {
+              // Navigator.of(context).pushNamed(Routes.appointment);
+            },
+            backgroundColor: theme.primaryContainer,
+            textColor: theme.onSurface,
+          ),
+          HomeGridItem(
+            title: 'Appointments',
+            icon: Image.asset(
+              'assets/icons/doctor-flaticon.png',
+              width: 25,
+              height: 25,
+            ),
+            onTap: () {
+              // Navigator.of(context).pushNamed(Routes.appointment);
+            },
+            backgroundColor: theme.primaryContainer,
+            textColor: theme.onSurface,
+          ),
+          HomeGridItem(
+            title: 'Appointments',
+            icon: Image.asset(
+              'assets/icons/doctor-flaticon.png',
+              width: 25,
+              height: 25,
+            ),
+            onTap: () {
+              // Navigator.of(context).pushNamed(Routes.appointment);
+            },
+            backgroundColor: theme.primaryContainer,
+            textColor: theme.onSurface,
+          ),
+          HomeGridItem(
+            title: 'Appointments',
+            icon: Image.asset(
+              'assets/icons/doctor-flaticon.png',
+              width: 25,
+              height: 25,
+            ),
+            onTap: () {
+              // Navigator.of(context).pushNamed(Routes.appointment);
+            },
+            backgroundColor: theme.primaryContainer,
+            textColor: theme.onSurface,
+          ),
+          HomeGridItem(
+            title: 'Appointments',
+            icon: Image.asset(
+              'assets/icons/doctor-flaticon.png',
+              width: 25,
+              height: 25,
+            ),
+            onTap: () {
+              // Navigator.of(context).pushNamed(Routes.appointment);
+            },
+            backgroundColor: theme.primaryContainer,
+            textColor: theme.onSurface,
+          ),
+        ]),
+  );
+}
