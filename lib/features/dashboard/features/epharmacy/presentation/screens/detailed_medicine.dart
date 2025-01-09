705c1b8133bc21e1fd014e8aaaa121ca92e86def
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-10 00:31:03 +0600

    refactor: replace custom shimmer loading implementation with a dedicated widget for better readability

--- lib/features/dashboard/features/epharmacy/presentation/screens/detailed_medicine.dart
+++ lib/features/dashboard/features/epharmacy/presentation/screens/detailed_medicine.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:meditouch/common/widgets/custom_loading_animation.dart';
 import 'package:meditouch/features/dashboard/features/epharmacy/logics/detailed_medicine_bloc.dart';
 import 'package:meditouch/features/dashboard/features/epharmacy/logics/detailed_medicine_event.dart';
 import 'package:meditouch/features/dashboard/features/epharmacy/logics/detailed_medicine_state.dart';
 import 'package:quickalert/quickalert.dart';
-import 'package:shimmer/shimmer.dart';
 
+import 'parts/detailed_medicine_shimmer.dart';
 import 'parts/detailed_medicine_sliver_appbar.dart';
 import 'parts/detailed_medicine_sliver_list.dart';
 
@@ -40,142 +39,7 @@ class DetailedMedicineScreen extends StatelessWidget {
           builder: (context, state) {
             if (state is DetailedMedicineLoading) {
               // return CustomLoadingAnimation(size: 30, color: theme.primary);
-              return SingleChildScrollView(
-                physics: const NeverScrollableScrollPhysics(),
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: [
-                    Shimmer.fromColors(
-                      baseColor: theme.primary.withOpacity(0.4),
-                      highlightColor: theme.secondary.withOpacity(0.6),
-                      child: Container(
-                        height: 300,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: theme.primary.withOpacity(0.1),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 12),
-                    Shimmer.fromColors(
-                      baseColor: theme.primary.withOpacity(0.4),
-                      highlightColor: theme.secondary.withOpacity(0.6),
-                      child: Container(
-                        height: 30,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: theme.primary.withOpacity(0.1),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Shimmer.fromColors(
-                      baseColor: theme.primary.withOpacity(0.4),
-                      highlightColor: theme.secondary.withOpacity(0.6),
-                      child: Container(
-                        height: 40,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: theme.primary.withOpacity(0.1),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Shimmer.fromColors(
-                      baseColor: theme.primary.withOpacity(0.4),
-                      highlightColor: theme.secondary.withOpacity(0.6),
-                      child: Container(
-                        height: 20,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: theme.primary.withOpacity(0.1),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 5),
-                    Shimmer.fromColors(
-                      baseColor: theme.primary.withOpacity(0.4),
-                      highlightColor: theme.secondary.withOpacity(0.6),
-                      child: Container(
-                        height: 20,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: theme.primary.withOpacity(0.1),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Shimmer.fromColors(
-                              baseColor: theme.primary.withOpacity(0.4),
-                              highlightColor: theme.secondary.withOpacity(0.6),
-                              child: Container(
-                                height: 40,
-                                width: 80,
-                                decoration: BoxDecoration(
-                                  color: theme.primary.withOpacity(0.1),
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 10),
-                            Shimmer.fromColors(
-                              baseColor: theme.primary.withOpacity(0.4),
-                              highlightColor: theme.secondary.withOpacity(0.6),
-                              child: Container(
-                                height: 30,
-                                width: 100,
-                                decoration: BoxDecoration(
-                                  color: theme.primary.withOpacity(0.1),
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Shimmer.fromColors(
-                          baseColor: theme.primary.withOpacity(0.4),
-                          highlightColor: theme.secondary.withOpacity(0.6),
-                          child: Container(
-                            height: 45,
-                            width: 120,
-                            decoration: BoxDecoration(
-                              color: theme.primary.withOpacity(0.1),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    Shimmer.fromColors(
-                      baseColor: theme.primary.withOpacity(0.4),
-                      highlightColor: theme.secondary.withOpacity(0.6),
-                      child: Container(
-                        height: 60,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: theme.primary.withOpacity(0.1),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              );
+              return detailedMedicineShimmer(theme);
             }
 
             if (state is DetailedMedicineError) {
