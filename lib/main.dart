401c4c72a8a413a8bcbe9b40df450910c6ec4a70
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-14 02:41:14 +0600

    feat: integrate GetX for state management and add detailed doctor controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meditouch/common/repository/hive_repository.dart';
+import 'package:meditouch/features/dashboard/features/doctors/data/repository/detailed_doctor_repository.dart';
+import 'package:meditouch/features/dashboard/features/doctors/logics/detailed_doctor_controller.dart';
 
 import 'app/meditouch_app.dart';
 import 'features/dashboard/features/account/logics/theme_cubit.dart';
@@ -24,6 +27,8 @@ void main() async {
   // theme
   final themeCubit = ThemeCubit();
   themeCubit.loadTheme();
+
+  
 
   // run the app
   runApp(MediTouchApp(
