1585b8f4eed02e0dd76bcb1107e9ba09c90b90d8
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-15 03:38:34 +0600

    refactor: enhance appointment fetching logic to include past appointments and add date parsing utility

--- lib/features/dashboard/features/appointments/data/repository/appointment_repository.dart
+++ lib/features/dashboard/features/appointments/data/repository/appointment_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 import '../../../doctors/data/models/appointment_model.dart';
 
 class AppointmentRepository {
@@ -22,8 +23,25 @@ class AppointmentRepository {
           final data = doc.data();
           final appointment = AppointmentModel.fromJson(data, doc.id);
 
+          // build the appointment date by parsing
+          // date:
+          final date = appointment.appointmentDate;
+          final time = appointment.appointmentTimeSlot.split('-')[0].trim();
+          final dateTime = '$date $time';
+
+          print('DateTime: $dateTime');
+
+          final appointmentDateTime = parseDateTime(dateTime);
+
+          print('AppointmentDateTime: $appointmentDateTime');
+
+          // check if the date is in the past
+          bool isPast = appointmentDateTime.isBefore(DateTime.now());
+
+          print('IsPast: $isPast');
+
           // Separate appointments based on the 'isCompleted' field
-          if (data['isCompleted'] == true) {
+          if (data['isCompleted'] == true || isPast) {
             pastAppointments.add(appointment);
           } else {
             upcomingAppointments.add(appointment);
@@ -31,8 +49,12 @@ class AppointmentRepository {
         }
 
         log('Appointments fetched successfully');
-        log('Upcoming: ${upcomingAppointments.first.toJson()}');
-        log('Past: $pastAppointments');
+        if (upcomingAppointments.isNotEmpty) {
+          log('Upcoming: ${upcomingAppointments.first.toJson()}');
+        } else {
+          log('No upcoming appointments.');
+        }
+
         yield {
           'status': true,
           'message': 'Appointments fetched successfully',
@@ -50,4 +72,13 @@ class AppointmentRepository {
       };
     }
   }
+}
+
+/// Parses a datetime string into a DateTime object.
+///
+/// The [dateTimeString] must follow the format "yyyy-MM-dd h:mm a".
+/// Returns the corresponding DateTime object.
+DateTime parseDateTime(String dateTimeString) {
+  final format = DateFormat("yyyy-MM-dd h:mm a"); // Define the pattern
+  return format.parse(dateTimeString);
 }
