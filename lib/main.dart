9202abaf625e3dd5cfed226b63cd0b70024610f1
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-15 02:47:36 +0600

    feat: enhance main function to initialize Hive repository and check user login status

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meditouch/common/repository/hive_repository.dart';
-import 'package:meditouch/features/dashboard/features/doctors/data/repository/detailed_doctor_repository.dart';
-import 'package:meditouch/features/dashboard/features/doctors/logics/detailed_doctor_controller.dart';
-
+import 'package:meditouch/common/utils/system_colors.dart';
 import 'app/meditouch_app.dart';
+import 'common/utils/welcome_util.dart';
 import 'features/dashboard/features/account/logics/theme_cubit.dart';
 
 void main() async {
@@ -19,7 +18,17 @@ void main() async {
 
   // initialize hive: local database
   await Hive.initFlutter();
-  await HiveRepository().init();
+  var hiveRepository = HiveRepository();
+
+  // initialize hive repository
+  await hiveRepository.init();
+
+  // get user info
+  var userInfo = await hiveRepository.getUserInfo();
+
+  bool isLoggedIn = userInfo != null &&
+      userInfo.containsKey('id') &&
+      (userInfo['id']?.toString().isNotEmpty ?? false);
 
   // load the .env file
   await dotenv.load(fileName: ".env");
@@ -28,10 +37,16 @@ void main() async {
   final themeCubit = ThemeCubit();
   themeCubit.loadTheme();
 
-  
+  bool darktheme = themeCubit.isDarkTheme();
+
+  changeSystemColor(darktheme);
+
+  bool isWelcomeWatched = await getWelcomePageWatched();
 
   // run the app
   runApp(MediTouchApp(
     themeCubit: themeCubit,
+    isLoggedIn: isLoggedIn,
+    isWelcomeWatched: isWelcomeWatched,
   ));
 }
