f1a3f41ca2f7ed22e6d9423933887b2c2b9c6a59
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-15 02:47:43 +0600

    feat: add utility function to change system colors based on theme

---
+++ lib/common/utils/system_colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void changeSystemColor(bool darkTheme) {
+  SystemChrome.setSystemUIOverlayStyle(darkTheme
+      ? SystemUiOverlayStyle(
+          statusBarColor: Color.alphaBlend(
+            const Color(0xFF470DC2).withOpacity(0.05), // Tint effect
+            Colors.white,
+          ),
+          statusBarIconBrightness: Brightness.dark,
+          systemNavigationBarColor: Color.alphaBlend(
+            const Color(0xFF470DC2).withOpacity(0.05), // Tint effect
+            Colors.white,
+          ),
+          systemNavigationBarIconBrightness: Brightness.dark,
+        )
+      : SystemUiOverlayStyle(
+          statusBarColor: Color.alphaBlend(
+            CupertinoColors.activeBlue.withOpacity(0.05), // Tint effect
+            const Color(0xFF141414),
+          ),
+          statusBarIconBrightness: Brightness.light,
+          systemNavigationBarColor: Color.alphaBlend(
+            CupertinoColors.activeBlue.withOpacity(0.05), // Tint effect
+            const Color(0xFF141414),
+          ),
+          systemNavigationBarIconBrightness: Brightness.light,
+        ));
+}
