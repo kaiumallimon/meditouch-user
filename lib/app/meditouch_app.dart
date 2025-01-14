397b5899b4a6a4a1c3d5701671c92947dc1732a6
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-15 02:47:46 +0600

    feat: update MediTouchApp to handle user login state and welcome page status in routing

--- lib/app/meditouch_app.dart
+++ lib/app/meditouch_app.dart
@@ -1,42 +1,12 @@
 import 'app_bloc_providers.dart';
 import './app_exporter.dart';
 
-// class MediTouchApp extends StatelessWidget {
-//   const MediTouchApp({super.key, required this.themeCubit});
-
-//   final ThemeCubit themeCubit;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiBlocProvider(
-//       providers: providers, // Initialize Blocs using MultiBlocProvider
-//       child: MaterialApp(
-//         debugShowCheckedModeBanner: false,
-//         theme: AppTheme().getTheme(),
-
-//         // Use GetX's navigation and routes
-//         initialRoute: "/",
-//         routes: {
-//           "/": (context) => const SplashScreen(),
-//           "/welcome": (context) => const WelcomeScreen(),
-//           "/login": (context) => LoginScreen(),
-//           "/register": (context) => const RegisterScreen(),
-//           "/dashboard": (context) => DashboardScreen(),
-//           "/profile": (context) => ProfileScreen(),
-//           "/theme": (context) => const ThemeScreen(),
-//           "/cart": (context) => const CartScreen(),
-//           "/orders": (context) => const OrderScreen(),
-//           "/doctors": (context) => const DoctorsScreen(),
-//         },
-//       ),
-//     );
-//   }
-// }
-
 class MediTouchApp extends StatelessWidget {
-  const MediTouchApp({super.key, required this.themeCubit});
+  const MediTouchApp({super.key, required this.themeCubit, required this.isLoggedIn, required this.isWelcomeWatched});
 
   final ThemeCubit themeCubit;
+  final bool isLoggedIn;
+  final bool isWelcomeWatched;
 
   @override
   Widget build(BuildContext context) {
@@ -49,9 +19,9 @@ class MediTouchApp extends StatelessWidget {
             theme: darkMode ? AppTheme().getDarkTheme() : AppTheme().getTheme(),
 
             // Use GetX's navigation and routes
-            initialRoute: "/",
+            initialRoute: isLoggedIn?  "/dashboard": isWelcomeWatched  ? "/login": "/welcome",
             routes: {
-              "/": (context) => const SplashScreen(),
+              // "/": (context) => const SplashScreen(),
               "/welcome": (context) => const WelcomeScreen(),
               "/login": (context) => LoginScreen(),
               "/register": (context) => const RegisterScreen(),
