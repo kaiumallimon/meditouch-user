111dd862ab52e8bd6b22f0da17aab9faa9a3121d
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-17 11:46:49 +0600

    feat: add searchMedicineForAutomaticOrder method to EpharmacyRepository for automatic medicine ordering

--- lib/features/dashboard/features/epharmacy/data/repository/epharmacy_repository.dart
+++ lib/features/dashboard/features/epharmacy/data/repository/epharmacy_repository.dart
@@ -39,4 +39,27 @@ class EpharmacyRepository {
       return null;
     }
   }
+
+
+  Future<List<Medicine>?> searchMedicineForAutomaticOrder(String query) async {
+    try {
+      final String url =
+          'https://api.medeasy.health/api/patient/search-medicines/?q=$query&from=0&to=10';
+
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final medicinesJson = jsonDecode(response.body)['medicines'] as List;
+        return medicinesJson
+            .map((medicineJson) => Medicine.fromJson(medicineJson))
+            .toList();
+      } else {
+        // Return an empty list if the status is not 200
+        return [];
+      }
+    } catch (e) {
+      print('Error: $e');
+      // Return an empty list in case of an error
+      return [];
+    }
+  }
 }
