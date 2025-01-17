b7956602ca293416f2f65c2b694418290c76f0ee
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-17 09:04:48 +0600

    feat: add AppSecrets class for storing application credentials

---
+++ lib/common/constants/secrets.dart
@@ -0,0 +1,8 @@
+class AppSecrets{
+  final String agoraAppId = '38fdac85a27f41059ebd21fe5b4f4135';
+  final String certificate= "e17072bfe1334ceb9393c865eca2c0e8";
+
+  final int zegoId = 460499814;
+  final String zegoSign = "673ff05e6de6558ed760b80ea1d698bb174644aef629d7e64e829e91e7fcfd57";
+
+}
