5fa2b9d37b33f63352d97ddfb88312dee2b89d35
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-17 12:06:21 +0600

    feat: add DoctorSearchScreen for searching and displaying doctors

---
+++ lib/features/dashboard/features/doctors/presentation/screens/doctor_search_screen.dart
@@ -0,0 +1,145 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:meditouch/common/widgets/custom_loading_animation.dart';
+import 'package:meditouch/features/dashboard/features/appointments/data/repository/review_repository.dart';
+import 'package:meditouch/features/dashboard/features/doctors/data/models/doctor_model.dart';
+import 'package:meditouch/features/dashboard/features/doctors/data/repository/doctor_repository.dart';
+import 'package:meditouch/features/dashboard/features/doctors/presentation/screens/doctor_detailed_page.dart';
+import 'package:quickalert/quickalert.dart';
+
+class DoctorSearchScreen extends StatefulWidget {
+  const DoctorSearchScreen({super.key});
+
+  @override
+  _DoctorSearchScreenState createState() => _DoctorSearchScreenState();
+}
+
+class _DoctorSearchScreenState extends State<DoctorSearchScreen> {
+  // Search controller and query
+  final TextEditingController _searchController = TextEditingController();
+  List<DoctorModel> _filteredDoctors = [];
+  List<DoctorModel> _allDoctors = [];
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: _searchController,
+          onChanged: _filterDoctors, // Call filter function on input change
+          decoration: InputDecoration(
+            hintText: 'Search for doctors',
+            hintStyle: TextStyle(color: theme.onSurface.withOpacity(.5)),
+            border: InputBorder.none,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: StreamBuilder<List<DoctorModel>>(
+          stream: DoctorRepository().getDoctors(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CustomLoadingAnimation(size: 25, color: theme.primary),
+              );
+            }
+            if (snapshot.hasError) {
+              return Center(child: Text('An error occurred'));
+            }
+
+            final doctors = snapshot.data!;
+
+            // Store all doctors for filtering
+            _allDoctors = doctors;
+
+            if (_filteredDoctors.isEmpty && _searchController.text.isEmpty) {
+              _filteredDoctors = doctors; // Display all if no filter
+            }
+
+            if (_filteredDoctors.isEmpty) {
+              return Center(child: Text('No doctors found'));
+            }
+
+            return ListView.builder(
+              itemCount: _filteredDoctors.length,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              itemBuilder: (context, index) {
+                final doctor = _filteredDoctors[index];
+                return GestureDetector(
+                  onTap: () async {
+                    QuickAlert.show(
+                      context: context,
+                      title: 'Loading',
+                      text: 'Please wait...',
+                      type: QuickAlertType.loading,
+                    );
+
+                    final rating = await ReviewRepository().calculateRating(doctor.id);
+
+                    Navigator.of(context).pop();
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DoctorDetailedPage(
+                          doctor: doctor,
+                          rating: rating,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: theme.primaryContainer,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: CachedNetworkImageProvider(doctor.imageUrl),
+                      ),
+                      title: Text(
+                        doctor.name,
+                        style: TextStyle(color: theme.primary, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        doctor.specialization,
+                        style: TextStyle(color: theme.onSurface.withOpacity(.5)),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios,
+                        color: theme.onSurface.withOpacity(.5),
+                        size: 15,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  // Function to filter doctors based on search input
+  void _filterDoctors(String query) {
+    setState(() {
+      if (query.isEmpty) {
+        _filteredDoctors = _allDoctors; // Show all doctors when query is empty
+      } else {
+        _filteredDoctors = _allDoctors.where((doctor) {
+          final queryLower = query.toLowerCase();
+          return doctor.name.toLowerCase().contains(queryLower) ||
+              doctor.email.toLowerCase().contains(queryLower) ||
+              doctor.phone.toLowerCase().contains(queryLower) ||
+              doctor.district.toLowerCase().contains(queryLower) ||
+              doctor.specialization.toLowerCase().contains(queryLower);
+        }).toList();
+      }
+    });
+  }
+}
